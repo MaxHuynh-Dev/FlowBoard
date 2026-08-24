f2554adc0b90624a714689c24184144b47bfd4e8
max <maxhuynh.work@gmail.com>
2026-08-24 17:40:08 +0700

    refactor: remove unused imports and clean up Header and HomePage components

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { ROUTER } from '@Constants/router';
 import type React from 'react';
-import { useCallback, useEffect, useState } from 'react';
 
 function Header(): React.ReactElement {
   return <header></header>;
--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,11 +1,20 @@
-'use client';
-
 import { Button } from '@mantine/core';
+import { cookies } from 'next/headers';
 import type React from 'react';
+import { createClient } from '@/utils/supabase/server';
 
-function HomePage(): React.ReactElement {
+async function HomePage(): Promise<React.ReactElement> {
+  const cookieStore = await cookies();
+  // const supabase = createClient(cookieStore);
+  // const { data: todos } = await supabase.from('todos').select();
+
   return (
     <div>
+      {/* <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul> */}
       <Button variant="filled">Button</Button>
     </div>
   );
