4c4900168e8d3d2545c58c86fd89e60c89e072c8
max <maxhuynh.work@gmail.com>
2026-08-26 14:04:24 +0700

    feat: enhance User component with Menubar for improved user experience and add handleLogout functionality in user store

--- src/layout/Sidebar/User/index.tsx
+++ src/layout/Sidebar/User/index.tsx
@@ -1,33 +1,48 @@
 'use client';
 
-import { Avatar, Text, UnstyledButton } from '@mantine/core';
+import { Avatar, Flex, Menu, Menubar, Text, UnstyledButton } from '@mantine/core';
+import { LogOut } from 'lucide-react';
 import type React from 'react';
 import { useUserStore } from '@/stores/userStore';
 import styles from './User.module.scss';
 
 export default function User(): React.ReactElement {
-  const user = useUserStore((state) => state.user);
+  const { user, handleLogout } = useUserStore((state) => state);
 
   return (
-    <UnstyledButton className={styles.user}>
-      <div className={styles.user_inner}>
-        <Avatar
-          src={user?.user_metadata.avatar_url || '/default-avatar.png'}
-          radius="xl"
-          size={35}
-          alt={user?.user_metadata.full_name || 'Guest User'}
-        />
+    <Menubar position="bottom-end">
+      <Menubar.Menu width={220}>
+        <Menubar.Target>
+          <div className={styles.user_inner}>
+            <Avatar
+              src={user?.user_metadata.avatar_url || '/default-avatar.png'}
+              radius="xl"
+              size={35}
+              alt={user?.user_metadata.full_name || 'Guest User'}
+            />
 
-        <div style={{ flex: 1 }}>
-          <Text size="sm" fw={500}>
-            {user?.user_metadata.full_name || 'Guest User'}
-          </Text>
+            <div style={{ flex: 1 }}>
+              <Text size="sm" fw={500}>
+                {user?.user_metadata.full_name || 'Guest User'}
+              </Text>
 
-          <Text c="dimmed" size="xs">
-            {user?.email}
-          </Text>
-        </div>
-      </div>
-    </UnstyledButton>
+              <Text c="dimmed" size="xs">
+                {user?.email}
+              </Text>
+            </div>
+          </div>
+        </Menubar.Target>
+        <Menubar.Dropdown>
+          <Menu.Item onClick={handleLogout}>
+            <Flex gap="md" justify="flex-start" align="center" wrap="wrap">
+              <LogOut size={24} />
+              <Text size="sm" fw={500}>
+                Log out
+              </Text>
+            </Flex>
+          </Menu.Item>
+        </Menubar.Dropdown>
+      </Menubar.Menu>
+    </Menubar>
   );
 }
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,14 +1,27 @@
 import type { User } from '@supabase/supabase-js';
 import { create } from 'zustand';
+import { ROUTERS } from '@/enums/router';
+import { createClient } from '@/utils/supabase/client';
 
 interface UserState {
   user: User | null;
   setUser: (user: User | null) => void;
   clearUser: () => void;
+  handleLogout: () => void;
 }
 
 export const useUserStore = create<UserState>((set) => ({
   user: null,
   setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null })
+  clearUser: () => set({ user: null }),
+  handleLogout: async () => {
+    try {
+      const supabase = createClient();
+      await supabase.auth.signOut();
+      set({ user: null });
+      window.location.href = ROUTERS.LOGIN;
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  }
 }));
